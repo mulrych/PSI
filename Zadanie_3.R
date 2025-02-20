33a2fd8f0a1332cf6270a2e5cac7f99bf5f9da4c
mulrych <m.ulrych@student.uw.edu.pl>
2025-02-20 08:53:19 +0100

    Add files via upload

---
+++ Zadanie_3.R
@@ -0,0 +1,9 @@
+# 3. Stwórz funkcję o nazwie pole_kola, która oblicza pole powierzchni koła dla danego promienia.
+
+pole_kola <- function (promien) {
+  pole <- pi*promien^2
+  return (pole)
+}
+
+pole_kola (3)
+
