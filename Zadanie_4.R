b5ee73f73a36222a13692f6499a730cdd8c39a1e
mulrych <m.ulrych@student.uw.edu.pl>
2025-02-20 08:59:05 +0100

    Add files via upload

---
+++ Zadanie_4.R
@@ -0,0 +1,10 @@
+# 4. Stwórz funkcję, która oblicza długość przeciwprostokątnej w trójkącie prostokątnym.
+
+przeciwprostokatna <- function (a,b){
+  
+  c <- sqrt(a^2+b^2)
+  return (c)
+}
+
+przeciwprostokatna (3,4)
+
